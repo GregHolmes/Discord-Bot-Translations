c91a28d59b39687c8e7daa6f930af02f29aa6cd0
Greg Holmes <greg.holmes2@vonage.com>
2020-10-13 15:05:01 +0100

    Added a default functionality for a Discord bot during first Twitch Stream

---
+++ index.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+require('dotenv').config();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+
+client.on('messageReactionAdd', async (reaction, user) => {
+	// When we receive a reaction we check if the reaction is partial or not
+	if (reaction.partial) {
+		// If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
+		try {
+			await reaction.fetch();
+		} catch (error) {
+			console.error('Something went wrong when fetching the message: ', error);
+			// Return as `reaction.message.author` may be undefined/null
+			return;
+		}
+  }
+
+	// Now the message has been cached and is fully available
+	console.log(`${reaction.message.author}'s message "${reaction.message.content}" gained a reaction!`);
+	// The reaction is now also fully available and the properties will be reflected accurately:
+  console.log(`${reaction.count} user(s) have given the same reaction to this message!`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
