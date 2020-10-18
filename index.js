8c22c1ffb38aae2a566254fd45829138c7cd838e
Greg Holmes <greg.holmes2@vonage.com>
2020-10-18 20:05:53 +0100

    Added functionality to translate text when a flag reaction is added to a message

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const AWS = require('aws-sdk');
+const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
+const flagService = require('./flags');
+
 require('dotenv').config();
+
+AWS.config.region = process.env.AWS_REGION;
+const translate = new AWS.Translate();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -23,12 +29,43 @@ client.on('messageReactionAdd', async (reaction, user) => {
 			// Return as `reaction.message.author` may be undefined/null
 			return;
 		}
-  }
+	}
 
-	// Now the message has been cached and is fully available
-	console.log(`${reaction.message.author}'s message "${reaction.message.content}" gained a reaction!`);
-	// The reaction is now also fully available and the properties will be reflected accurately:
-  console.log(`${reaction.count} user(s) have given the same reaction to this message!`);
+	translateText(reaction.message.content, flagService.convertFlagToText(reaction.emoji.name), reaction);
+
 });
+
+const translateText = async (originalText, targetLanguageCode, reaction) => {
+	if (targetLanguageCode === null) {
+		return;
+	}
+
+	return new Promise((resolve, reject) => {
+		let params = {
+			Text: originalText,
+			SourceLanguageCode: "auto",
+			TargetLanguageCode: targetLanguageCode
+		}
+
+		try {
+			translate.translateText(params, (err, data) => {
+				if (err) {
+					reject(err);
+				}
+
+				if (data) {
+					reaction.message.channel.send(reaction.message.author.username + ' requested a translation from `' + data.SourceLanguageCode + '` to `' + data.TargetLanguageCode + '`');
+					reaction.message.channel.send('> ' + originalText);
+					reaction.message.channel.send('```' + data.TranslatedText + '```');
+					resolve(data);
+				}
+
+				return data;
+			});
+		} catch (err) {
+			console.log(err);
+		}
+	});
+}
 
 client.login(process.env.DISCORD_TOKEN);
